c7ebb89bcc2672e87e0d20f188ed8b3f46107319
lq <lfszliqiang@163.com>
2019-08-15 18:31:39 +0800

    第二次:捕获退出事件

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame
 from pygame.locals import *
 import sys
+from util.local import *
 
 # 初始化游戏界面
 pygame.init()
 # 创建游戏窗口
-pygame.display.set_mode((300,300))
+window = pygame.display.set_mode((13*SIZE,13*SIZE))
 
 
 
 while True:
-    pass
+
+    # 捕获事件
+    eventList = pygame.event.get()
+    for eventEle in eventList:
+        if eventEle.type == QUIT:
+            # 退出游戏
+            pygame.quit()
+            # 退出程序
+            sys.exit()
