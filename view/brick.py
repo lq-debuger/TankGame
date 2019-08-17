b6251bdf02af90050199faeebc58f200f744bcb8
lq <lfszliqiang@163.com>
2019-08-17 11:08:25 +0800

    第十六次:砖墙受到足够伤害后销毁

--- view/brick.py
+++ view/brick.py
@@ -2,8 +2,9 @@ import pygame
 from base.view import *
 from base.block import BlockAble
 from base.suffer import SufferAble
+from base.destry import DestroyAble
 
-class Brick(Views,BlockAble,SufferAble):
+class Brick(Views,BlockAble,SufferAble,DestroyAble):
 
     def __init__(self,**kwargs):
         super(Brick, self).__init__(**kwargs,img='./img/walls.gif')
@@ -12,6 +13,13 @@ class Brick(Views,BlockAble,SufferAble):
         self.height = self.image.get_height()
         # 设置血量值
         self.hp = 3
+        # 设置销毁状态
+        self.shouldDestroy = False
 
     def notifySuffer(self):
         self.hp -= 1
+
+    def needDestroy(self):
+        if self.hp <= 0:
+            self.shouldDestroy = True
+            return self.shouldDestroy
